4fa0c6d4d0267339abb0323a65d4644d86b75490
Roman <romanmikhailenko00@gmail.com>
2021-09-21 22:02:10 +0600

    first task is done

---
+++ firstlab/src/main/java/net/omsu/firstlab/Main.java
@@ -0,0 +1,12 @@
+package net.omsu.firstlab;
+
+import picocli.CommandLine;
+import java.io.IOException;
+
+public class Main {
+    public static void main(final String[] args) throws IOException {
+        System.exit(new CommandLine(new BaseCommands())
+                .setCaseInsensitiveEnumValuesAllowed(true)
+                .execute(args));
+    }
+}
