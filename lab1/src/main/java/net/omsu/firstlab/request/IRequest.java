882d1236a68f3479601357966493a2471eee4ad9
Roman <romanmikhailenko00@gmail.com>
2021-09-21 22:06:02 +0600

    renamed folder

---
+++ lab1/src/main/java/net/omsu/firstlab/request/IRequest.java
@@ -0,0 +1,14 @@
+package net.omsu.firstlab.request;
+
+import java.io.File;
+import java.net.URL;
+
+public interface IRequest {
+
+    String pull(URL url, File outputBody);
+
+    default String getUserAgent() {
+        return "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
+                "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36";
+    }
+}
